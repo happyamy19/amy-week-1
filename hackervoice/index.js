bc38ca007ace07011f0d689ebfb70829880d3713
Amelia Churchill <ameliachurchill@Amelias-Air.hsd1.il.comcast.net>
2021-06-25 19:05:30 -0500

    added conditionals

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,8 +2,17 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var password = req.query.password
+
+    var repsonse;
+    if (password == "letmein"){
+        response = "Access Granted"
+    }
+    else{
+        response = "Access Denied"
+    }
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: response
     };
 }
