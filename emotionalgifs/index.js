b959b0a8ac0ce0eda50ea21396b82635139a8062
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2021-07-05 19:00:42 -0500

    created a new emo gifs bc the last one was not updating...

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,21 +1,38 @@
 //help me please
 var multipart = require('parse-multipart');
-
+var fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
-
-// here's your boundary:
-var boundary = multipart.getBoundary(req.headers['content-type']);
-  
-// TODO: assign the body variable the correct value
-var body = req.body
-
-// parse the body
-var parts = multipart.Parse(body, boundary);
-let base64data = Buffer.from(parts[0].data).toString('base64')
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    var body = req.body
+    var parts = multipart.Parse(body, boundary);
+    var image = parts[0].data
+    var result = analyzeImage(image)
     
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: base64data
+        body: {result}
     };
 }
+
+    async function analyzeImage(img){
+    
+        const subscriptionKey = process.env.FACEAPI_KEY1;
+        const uriBase = process.env.FACEAPI_ENDPOINT + '/face/v1.0/detect';
+        let params = new URLSearchParams({
+            'returnFaceId': 'true',
+            'returnFaceAttributes': 'emotion'
+        })
+        //COMPLETE THE CODE
+        let resp = await fetch(uriBase + '?' + params.toString(), {
+            method: 'POST',  //WHAT TYPE OF REQUEST?
+            body: img,  //WHAT ARE WE SENDING TO THE API?
+            headers: {
+                'Content-Type': 'application/octet-stream', 
+                'Ocp-Apim-Subscription-Key': subscriptionKey
+            }
+        })
+        let data = await resp.json();
+        
+        return data; 
+    }
