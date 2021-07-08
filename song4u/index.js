8394b5176d4fe28c671d9dd99f5426f90a0adbb1
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2021-07-07 21:23:40 -0500

    updated good4u

--- song4u/index.js
+++ song4u/index.js
@@ -1,18 +1,63 @@
 const querystring = require('querystring');
+const fetch = require('node-fetch'); // use to make requests 
 
 module.exports = async function (context, req) {
     //user messaged num, message is returned as req.body
     context.log(req.body);
-
-
     const queryObject = querystring.parse(req.body);
     const url = queryObject.MediaUrl0;
 
+    let resp = await fetch(url, {
+        method: 'GET'
+    })
 
+    let data = await resp.arrayBuffer() //data holds the image we just downloaded
+    let age_data = analyzeImage(data)
+    let age = age_data[0].faceAttributes.age
+
+    let generation = determine_generation(age)
+    context.log(generation)
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: url
+        body: generation
     };
 }
 //number: +18434179543
+
+async function analyzeImage(img){
+    
+    const subscriptionKey = process.env.FACEAPI_KEY1;
+    const uriBase = process.env.FACEAPI_ENDPOINT + '/face/v1.0/detect';
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'age'
+    })
+    //COMPLETE THE CODE
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  //WHAT TYPE OF REQUEST?
+        body: img,  //WHAT ARE WE SENDING TO THE API?
+        headers: {
+            'Content-Type': 'application/octet-stream', 
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+    let data = await resp.json();
+    return data; 
+}
+
+function determine_generation(age){
+    let generation; 
+    if (age > 5 && age < 25){
+        generation = "GenZ"
+    } else if (age > 24 && age < 41){
+        generation  = "GenY"
+    } else if (age > 40 && age < 57){
+        generation  = "GenX"
+    } else if (age > 56 && age < 76){
+        generation  = "BabyBoomers"
+    } else {
+        generation = "unknown"
+    }
+    return generation
+}
