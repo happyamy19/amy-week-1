3445f2ff9be926d181e0a48b96bac881149307c6
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2021-07-07 23:05:51 -0500

    changed song to songs

--- song4u/index.js
+++ song4u/index.js
@@ -24,7 +24,7 @@ module.exports = async function (context, req) {
 "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
 "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
 
-let value = song[generation]
+let value = songs[generation]
 let response = `We guessed you're part of this generation: ${generation}! Happy listening! ${value}`
 
 context.log(response)
