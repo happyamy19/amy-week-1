8b37e5de816a061325f6eb03270f49688ad26ee6
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2021-07-22 12:45:00 -0500

    Update prog

---
+++ bunnimage/script.js
@@ -0,0 +1,9 @@
+function getImage(event){
+    event.preventDefault()
+    if (document.getElementById("name").value != ''){
+        $('#output').text("Thanks!")
+    }
+    else{ 
+        alert("No name error.")
+    }
+}
