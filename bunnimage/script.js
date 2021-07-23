0f97dab5135bbfeb379df03c559e6104ed988733
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2021-07-23 15:00:24 -0500

    added i in image to html

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,30 +1,29 @@
 function getImage(event){
     event.preventDefault()
 
-    const bunniForm = document.getElementById("myform");
+    const myfrom = document.getElementById("myform");
     let nameInput = document.getElementById("name")
-    let image = document.getElementById("image")
-    const file = image.files[0]; // fileInput is the file upload input element
+    let fileInput = document.getElementById("image")
+    const file = fileInput.files[0]; // fileInput is the file upload input element
     
     // get image uploaded, save in payload
-    const payload = new FormData(bunniForm);
+    const payload = new FormData(myform);
     payload.append("file", file);
 
     console.log(payload)
     if (document.getElementById("name").value != ''){
-
         try{
             let url = "https://week-1.azurewebsites.net/api/bunnimage-upload"
-            $('#output').text("Thanks!")
              console.log("Image was uploaded, making post req to azure func");
             // create request to azure function!
             const resp = fetch(url, {
                 method: 'POST',
                 headers: {
-                    'codename': username
+                    'codename': nameInput.value // was username
                 },
                 body: payload
                 });
+                console.log("post successful")
         $('#output').text("Your image has been stored successfully!")
         
         } catch (err){
